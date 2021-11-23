657f3448018729a25f8541f2c97b822eb3f3b7ac
Maria Aley <mjaleyj@gmail.com>
2021-11-23 21:21:18 +0100

    Configure bootstrap in Django

--- energyconservation/settings.py
+++ energyconservation/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'energyconservation.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates/student'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -123,6 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = ['static']
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
--- student/models.py
+++ student/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 # Create your models here.
 class Student(models.Model):
-    name = models.CharField(max_length=80)
+    first_name = models.CharField(max_length=80, default='Maria')
+    last_name = models.CharField(max_length=80, default='Aley')
     age = models.IntegerField()
+    email = models.CharField(max_length=80, default='maria@gmail.com')
 
